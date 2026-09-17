ee0864d442eb06160b2621bb9a0cdc1ff2a47544
Ion Beristain <jonboyka@gmail.com>
2026-09-17 14:11:20 +0200

    Ariketa 8: XML bidezko erabiltzaile-interfazea

---
+++ Ariketa8_XmlInterfazea/app/src/main/java/com/example/ariketa8_xmlinterfazea/NireJarduera.java
@@ -0,0 +1,24 @@
+package com.example.ariketa8_xmlinterfazea;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class NireJarduera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.jarduera_nirea);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
